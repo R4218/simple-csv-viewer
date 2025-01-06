4f07e8ee86e9af6fe83ac9128923f39ff84d35ac
Razibur <rahman.razibur@gmail.com>
2025-01-06 19:35:17 +0600

    feat: integrate Google Analytics tracking into the application

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
 import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Tag (gtag.js) */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-YFXRCM9MBK"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YFXRCM9MBK');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
